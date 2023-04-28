4f7182fe573144a2d4b94a58c65f464b728d3483
NatashechkaS <127501647+NatashechkaS@users.noreply.github.com>
2023-04-28 19:05:46 +0300

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Main {
 
-
     public static void main(String[] args) {
+
         System.out.println("Выберите порядковый номер студента: ");
-        String text = """
-                1. Мицик Матвей Антонович
-                2. Воронова Юлия Павловна
-                3. Кузьмин Вячеслав Александрович
-                4. Пашков Дмитрий Сергеевич
-                5. Орлова Мария Васильевна
-                6. Сусликов Иван Михайлович
-                """;
-        System.out.println(text);
+        System.out.println(getListOfStudents());
         Scanner console = new Scanner(System.in);
         String ch1 = "1";
         int x = 0;
-        String[] phones = new String[0];
         System.out.println("Для завершения вывода информации введите - stop");
+        ArrayList<String> numbers = getPhones();
         while (true) {
             try {
                 ch1 = console.nextLine();
@@ -30,27 +23,43 @@ public class Main {
                     break;
                 }
                 x = Integer.parseInt(ch1);
-                System.out.println(getPhonePhones(phones, x - 1));
+                System.out.println(getPhoneNumber(numbers, x - 1));
 
             } catch (Exception e) {
-                System.out.println("Неверный формат данных!" + e.getMessage());
+                System.out.println("Неверный формат данных! " + e.getMessage());
             }
         }
     }
 
-    static String getPhoneNumber(String[] phones, int i) {
-        return (phones[i]);
+    static String getPhoneNumber(ArrayList<String> phones, int i) {
+        return phones.get(i);
     }
-    static String getPhonePhones(String[] phones, int i) {
-        if (phones == null) {
-            phones = new String[6];
-            phones[0] = "+7910-875-37-24";
-            phones[1] = "+7910-357-29-54";
-            phones[2] = "+7910-987-65-43";
-            phones[3] = "+7915-967-65-41";
-            phones[4] = "+7910-900-65-90";
-            phones[5] = "+7910-777-65-65";
-        }
-        return (phones[i]);
+
+    static ArrayList<String> getPhones() {
+
+        ArrayList<String> phones = new ArrayList<String>();
+        phones.add("+7910-875-37-24");
+        phones.add("+7910-357-29-54");
+        phones.add("+7910-987-65-43");
+        phones.add("+7915-967-65-41");
+        phones.add("+7910-900-65-90");
+        phones.add("+7910-777-65-65");
+
+        return phones;
     }
+
+    static ArrayList<String> getListOfStudents() {
+
+        ArrayList<String> listOfStudents = new ArrayList<String>();
+
+        listOfStudents.add("1. Мицик Матвей Антонович");
+        listOfStudents.add("\n2. Воронова Юлия Павловна");
+        listOfStudents.add("\n3. Кузьмин Вячеслав Александрович");
+        listOfStudents.add("\n4. Пашков Дмитрий Сергеевич");
+        listOfStudents.add("\n5. Орлова Мария Васильевна");
+        listOfStudents.add("\n6. Сусликов Иван Михайлович");
+
+        return listOfStudents;
+    }
+
 }
